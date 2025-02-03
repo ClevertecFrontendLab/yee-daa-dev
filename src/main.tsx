ef9e47ac123487c10973c68b78c59a127a4f3c55
Aliaksei Olesik <AOlesik@alfabank.ru>
2025-02-03 13:46:53 +0300

    feat(*): add react-router, wrap main to router and chakra provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import './index.css';
 
+import { ChakraProvider } from '@chakra-ui/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
 
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <ChakraProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ChakraProvider>
     </StrictMode>,
 );
