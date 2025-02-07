914ad48093aafb4a2142ee0e073524d4ddb25eca
Aliaksei Olesik <AOlesik@alfabank.ru>
2025-02-07 18:47:58 +0300

    feat(*): complete navigation

--- src/components/side-nav/nav-item.tsx
+++ src/components/side-nav/nav-item.tsx
@@ -1,6 +1,6 @@
 import { AccordionIcon, AccordionItem, AccordionPanel, Image } from '@chakra-ui/icons';
 import { AccordionButton, HStack, Text } from '@chakra-ui/react';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { NavLink } from 'react-router';
 
 import { categoriesMap } from '../../constants/categories.ts';
@@ -8,22 +8,38 @@ import { MenuItem } from '../../constants/nav-menu.ts';
 import { SubNavItem } from './sub-nav-item.tsx';
 
 export const NavItem: FC<MenuItem> = ({ subItems, path, title }) => {
+    const [isActive, setIsActive] = useState(false);
     return (
-        <AccordionItem>
-            <NavLink to={path}>
+        <AccordionItem border='none'>
+            <NavLink
+                to={path}
+                className={({ isActive }) => {
+                    setIsActive(isActive);
+
+                    return undefined;
+                }}
+            >
                 <AccordionButton
+                    padding='8px 12px'
                     _expanded={{
-                        background: 'lime.50',
+                        background: 'lime.100',
                     }}
                 >
-                    <HStack as='span' flex='1' textAlign='left'>
-                        <Image src={categoriesMap[title]} alt={title} />
-                        <Text>{title}</Text>
+                    <HStack as='span' flex='1' textAlign='left' spacing={3}>
+                        <Image src={categoriesMap[title]} alt={title} w={6} h={6} />
+                        <Text
+                            fontSize='md'
+                            lineHeight={6}
+                            fontWeight={isActive ? 700 : 500}
+                            noOfLines={1}
+                        >
+                            {title}
+                        </Text>
                     </HStack>
                     <AccordionIcon />
                 </AccordionButton>
             </NavLink>
-            <AccordionPanel>
+            <AccordionPanel padding='4px 8px 4px 40px'>
                 {subItems?.map((el) => <SubNavItem key={el.path} {...el} />)}
             </AccordionPanel>
         </AccordionItem>
--- src/components/side-nav/sub-nav-item.tsx
+++ src/components/side-nav/sub-nav-item.tsx
@@ -1,24 +1,30 @@
-import { Box } from '@chakra-ui/react';
+import './side-nav.module.css';
+
+import { Box, Text } from '@chakra-ui/react';
 import { FC, useState } from 'react';
 import { NavLink } from 'react-router';
 
 import { MenuItem } from '../../constants/nav-menu.ts';
 
 export const SubNavItem: FC<MenuItem> = ({ path, title }) => {
-    const [isSecondLvlActive, setIsSecondLvlActive] = useState(false);
+    const [isActive, setIsActive] = useState(false);
+
+    const cn = ['sideNavItem', isActive ? 'active' : ''].join(' ').trim();
 
     return (
         <NavLink
             to={path}
             key={path}
             className={({ isActive }) => {
-                setIsSecondLvlActive(isActive);
+                setIsActive(isActive);
 
                 return undefined;
             }}
         >
-            <Box borderColor='lime.400' borderLeftWidth={isSecondLvlActive ? 4 : 2}>
-                {title}
+            <Box borderColor='lime.400' borderLeftWidth={'1px'} pl={3} mt={1} mb={1} className={cn}>
+                <Text fontSize='md' lineHeight={6} fontWeight={isActive ? 700 : 500} noOfLines={1}>
+                    {title}
+                </Text>
             </Box>
         </NavLink>
     );
