ecc6c46257d75037315131ad4a01ec3f397ebf7a
Aliaksei Olesik <AOlesik@alfabank.ru>
2025-02-08 13:57:46 +0300

    fix(*): remove useless test case

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -5,13 +5,6 @@ describe('App Component', () => {
         cy.visit('http://localhost:3000');
     });
 
-    it('should increase the count when button is clicked', () => {
-        cy.contains('count is 0');
-        // Нажатие на кнопку
-        cy.get('button').click();
-        cy.contains('count is 1');
-    });
-
     it('should take a screenshot of the app', () => {
         cy.screenshot('app-screenshot');
     });
