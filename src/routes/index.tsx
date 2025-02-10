85c385a7b58199176b841f34fe8ecc6e3d3e73fc
Aliaksei Olesik <AOlesik@alfabank.ru>
2025-02-10 10:30:36 +0300

    feat(routes): add recursive routes mapping

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,30 +1,33 @@
+import { ReactNode } from 'react';
 import { Route, Routes } from 'react-router';
 
 import { Layout } from '../components/layout';
-import { navMenu } from '../constants/nav-menu.ts';
+import { MenuItem, navMenu } from '../constants/nav-menu.ts';
 import { Paths } from '../constants/path.ts';
 import { JuiciestPage } from '../pages/juiciest-page';
 import { MainPage } from '../pages/main-page';
 import { VeganPage } from '../pages/vegan-page';
+
+const renderRoutes = (routes: MenuItem[]) => {
+    return routes.map((item): ReactNode => {
+        if (item.subItems)
+            return (
+                <>
+                    <Route key={item.path} path={item.path} element={<VeganPage />} />
+                    {renderRoutes(item.subItems)}
+                </>
+            );
+
+        return <Route key={item.path} path={item.path} element={<VeganPage />} />;
+    });
+};
 
 export const routes = (
     <Routes>
         <Route path={Paths.R_SWITCHER} element={<Layout />}>
             <Route index element={<MainPage />} />
             <Route path={Paths.JUICIEST} element={<JuiciestPage />} />
-            {navMenu.map((item) => {
-                if (item.subItems)
-                    return (
-                        <>
-                            <Route key={item.path} path={item.path} element={<VeganPage />} />
-                            {item.subItems.map(({ path }) => (
-                                <Route key={path} path={path} element={<VeganPage />} />
-                            ))}
-                        </>
-                    );
-
-                return <Route key={item.path} path={item.path} element={<VeganPage />} />;
-            })}
+            {renderRoutes(navMenu)}
         </Route>
     </Routes>
 );
