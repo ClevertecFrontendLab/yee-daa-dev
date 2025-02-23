b0f742bdcc455e835e1b7347f8e95a1c016a09f3
skustov <skustov@clevertec.ru>
2025-02-23 13:02:41 +0300

    fix(*): fix paths alias settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,19 +5,33 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs', 'coverage'],
+    ignorePatterns: ['dist', '.eslintrc.cjs', 'coverage', 'node_modules'],
     parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh', 'import', 'simple-import-sort'],
+    plugins: ['react', 'react-refresh', 'import', 'simple-import-sort'],
     rules: {
         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
         'import/order': [
             'error',
             {
+                pathGroups: [
+                    {
+                        pattern: '~/**',
+                        group: 'internal',
+                        position: 'after',
+                    },
+                    {
+                        pattern: '@public/**',
+                        group: 'internal',
+                        position: 'after',
+                    },
+                ],
                 groups: [['builtin', 'external'], ['internal'], ['parent', 'sibling', 'index']],
                 'newlines-between': 'always',
             },
         ],
         'simple-import-sort/imports': 'error',
         'simple-import-sort/exports': 'error',
+        'arrow-body-style': 'error',
+        'react/jsx-curly-brace-presence': ['error', 'never'],
     },
 };
