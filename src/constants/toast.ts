fbe699305c61f2214052bc4daedc71b3fc73bd94
Nikita Kisly <nkisly@clevertec.ru>
2025-03-23 21:03:47 +0300

    feat(sign-up): add sign-up interaction

--- src/constants/toast.ts
+++ src/constants/toast.ts
@@ -1,10 +1,25 @@
+import { HttpStatus } from './http-status';
+
 export const TOAST_MESSAGE = {
-    signInError: {
-        id: 'sign-in-error',
-        title: 'Неверный логин или пароль',
-        description: 'Попробуйте снова',
+    SignInToast: {
+        [HttpStatus.UNAUTHORIZED]: {
+            id: 'sign-in-error-credentials',
+            title: 'Неверный логин или пароль',
+            description: 'Попробуйте снова',
+        },
+        [HttpStatus.FORBIDDEN]: {
+            id: 'sign-in-error-not-verified',
+            title: 'E-mail не верифицирован',
+            description: 'Проверьте почту и перейдите по ссылке',
+        },
     },
-    serverError: {
+    SignUpToast: {
+        [HttpStatus.OK]: {
+            id: 'sign-up-verified-ok',
+            title: 'Верификация прошла успешно',
+        },
+    },
+    ServerErrorToast: {
         id: 'server-error',
         title: 'Ошибка сервера',
         description: 'Попробуйте немного позже',
