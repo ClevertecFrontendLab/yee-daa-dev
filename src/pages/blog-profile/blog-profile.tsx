df3a5675c90bd9773aa61a586a73903fa4b608b2
Yuliya Lobzha <yulia.lobzha@gmail.com>
2025-06-14 15:49:20 +0300

    fix(*): add avatar link

--- src/pages/blog-profile/blog-profile.tsx
+++ src/pages/blog-profile/blog-profile.tsx
@@ -37,6 +37,7 @@ export const BlogProfile = () => {
                 mt={{ base: 0, md: -8, xmd: -4 }}
                 minWidth={{ base: 0, sm: 356, xmd: 422, '2xl': 480 }}
                 _id={info.bloggerInfo._id}
+                imgSrc={info.bloggerInfo.photoLink}
                 dataTestId='blogger-user-info'
             />
             {isArrayWithItems(data.recipes) && (
